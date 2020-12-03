173cceb179d86c1a748c01901b2a26c2f76173d6
bianca.sozzi <bianca.sozzi@klarna.com>
2020-12-03 10:05:00 +0100

    create components + angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogoComponent,
+    RegistroComponent,
+    LoginComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/registro/registro.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  registroForm: FormGroup;
+  constructor(private fb:FormBuilder) {
+      this.registroForm=this.fb.group({
+          nombre: ['',[Validators.required]],
+          apellido: ['',[Validators.required]],
+          telefono: ['', Validators.pattern("[0-9]")],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
